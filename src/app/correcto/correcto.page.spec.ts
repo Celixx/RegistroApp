d6265efba68d1e7b9d79029535caa4da98cb3fca
TaintedNobody <carl.uribe@duocuc.cl>
2023-09-06 18:59:11 -0300

    correo.html, pregunta.html y correcto.html

---
+++ src/app/correcto/correcto.page.spec.ts
@@ -0,0 +1,17 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { CorrectoPage } from './correcto.page';
+
+describe('CorrectoPage', () => {
+  let component: CorrectoPage;
+  let fixture: ComponentFixture<CorrectoPage>;
+
+  beforeEach(waitForAsync () => {
+    fixture = TestBed.createComponent(CorrectoPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});

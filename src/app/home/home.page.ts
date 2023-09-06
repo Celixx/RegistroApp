6c049cf2e504e82619c9f4ada821fd9f22f8d1b6
Celix <alonsomurua@gmail.com>
2023-09-06 20:23:10 -0300

    funcionalidad de inicio de sesion

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+import { AnimationController} from '@ionic/angular';
+import { Usuario } from 'src/app/usuario';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +11,36 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public usuario: Usuario;
+
+  constructor(private activeroute: ActivatedRoute
+  , private router: Router
+  , private alertController: AlertController
+  , private animationController: AnimationController) {
+
+    this.usuario = new Usuario('', '', '', '', '', '', 0, null);
+
+    this.activeroute.queryParams.subscribe(params => { 
+
+      const nav = this.router.getCurrentNavigation();
+      if (nav) {
+        // Si tiene datos extra, se rescatan y se asignan a una propiedad
+        if (nav.extras.state) {
+          this.usuario = nav.extras.state['usuario'];
+          return;
+        }
+      }
+      // Si no vienen datos extra desde la página anterior, quiere decir que el usuario
+      // intentó entrar directamente a la página home sin pasar por el login,
+      // de modo que el sistema debe enviarlo al login para que inicie sesión.
+      this.router.navigate(['/login']);
+
+    });
+
+  }
+
+  public ngOnInit(): void {
+
+  }
 
 }
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -10,11 +10,46 @@ import { Router, NavigationExtras } from '@angular/router';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  public usuario: Usuario;
 
-  ngOnInit() {
+  constructor(private router: Router, private toastController: ToastController) {
+    this.usuario = new Usuario('', '', '', '', '', '', 0, null)
   }
 
-  public ingresar(): void {}
+  public ngOnInit(): void {
+  }
+
+  public ingresar(): void {
+    // Validación del usuario
+    if (this.usuario) {
+      const mensajeError = this.usuario.validarUsuario();
+      if (mensajeError) {
+        this.mostrarMensaje(mensajeError);
+      }
+      // Búsqueda del usuario validado
+      const usuarioLog: Usuario | undefined = this.usuario.buscarUsuarioValido(this.usuario.correo, this.usuario.password);
+
+      if(usuarioLog){
+        const navigationExtras: NavigationExtras = {
+          state: {
+            usuario: usuarioLog
+          }
+        };
+        // Mensaje toast
+        this.mostrarMensaje(`Inicio de sesión exitoso, Bienvenido(a) ${usuarioLog.nombre} ${usuarioLog.apellido}`);
+        // Redirect
+        this.router.navigate(['/home'],navigationExtras)
+      }
+    }
+  }
+
+  async mostrarMensaje(mensaje: string, duracion?: number) {
+
+    const mensajeToast = await this.toastController.create({
+      message: mensaje,
+      duration: duracion? duracion: 3000
+    });
+    mensajeToast.present();
+  }
 
 }

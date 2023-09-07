56fe6df686cbec64fe4b6239609032f06b06694c
Celix <alonsomurua@gmail.com>
2023-09-06 22:46:07 -0300

    implementación funcional qr

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -4,6 +4,7 @@ import { AlertController } from '@ionic/angular';
 import { AnimationController} from '@ionic/angular';
 import { Usuario } from 'src/app/usuario';
 import { NavigationExtras } from '@angular/router';
+import { Asistencia } from 'src/app/asistencia';
 import jsQR, { QRCode } from 'jsqr';
 
 @Component({
@@ -13,8 +14,16 @@ import jsQR, { QRCode } from 'jsqr';
 })
 export class HomePage {
 
+  @ViewChild('video')
+  private video!: ElementRef;
+
+  @ViewChild('canvas')
+  private canvas!: ElementRef;
+
   public usuario: Usuario;
   public escaneando = false;
+  public asistencia: Asistencia = new Asistencia();
+  public datosQR: string = '';
 
   constructor(private activeroute: ActivatedRoute
   , private router: Router
@@ -56,8 +65,62 @@ export class HomePage {
     this.router.navigate(['/mi-clase'],navigationExtras)
   }
 
-  public scanQR(): void {
+  public async scanQR() {
 
+    const mediaProvider: MediaProvider = await navigator.mediaDevices.getUserMedia({
+      video: {facingMode: 'environment'}
+    });
+    this.video.nativeElement.srcObject = mediaProvider;
+    this.video.nativeElement.setAttribute('playsinline', 'true');
+    this.video.nativeElement.play();
+    this.escaneando = true;
+    requestAnimationFrame(this.verificarVideo.bind(this));
+
+  }
+
+  async verificarVideo() {
+    if (this.video.nativeElement.readyState === this.video.nativeElement.HAVE_ENOUGH_DATA) {
+      if (this.obtenerDatosQR() || !this.escaneando) return;
+      requestAnimationFrame(this.verificarVideo.bind(this));
+    } else {
+      requestAnimationFrame(this.verificarVideo.bind(this));
+    }
+  }
+
+  public obtenerDatosQR(): boolean {
+    const w: number = this.video.nativeElement.videoWidth;
+    const h: number = this.video.nativeElement.videoHeight;
+    this.canvas.nativeElement.width = w;
+    this.canvas.nativeElement.height = h;
+    const context: CanvasRenderingContext2D = this.canvas.nativeElement.getContext('2d');
+    context.drawImage(this.video.nativeElement, 0, 0, w, h);
+    const img: ImageData = context.getImageData(0, 0, w, h);
+    let qrCode: QRCode | null = jsQR(img.data, w, h, { inversionAttempts: 'dontInvert' });
+    if (qrCode) {
+      if (qrCode.data !== '') {
+        this.escaneando = false;
+        this.mostrarDatosQROrdenados(qrCode.data);
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public mostrarDatosQROrdenados(datosQR: string): void {
+    this.datosQR = datosQR;
+    const objetoDatosQR = JSON.parse(datosQR);
+    // ----------------------------------
+    // TAREA PARA COMPLETAR POR EL ALUMNO
+    // ----------------------------------
+    // 1) Ejecutar el setter de la clase Asistencia:
+    //     this.asistencia.setAsistencia(...parametros...)
+    //    de modo que los parámetros los tome del objeto datosQR,
+    //    por ejemplo: datosQR.nombreAsignatura contiene el valor 
+    //    del nombre de la asignatura en la cual el alumno
+    //    debe quedar presente.
+    // 2) Hacer una interpolación entre las propiedades 
+    //    de "this.asistencia" y la página HTML, de modo
+    //    que la página muestre de manera ordenada estos datos.
   }
 
   public stopScanQR(): void {
